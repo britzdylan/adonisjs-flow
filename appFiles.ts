2f8fe55deb19235d64eabd3e286b211218e08520
Dylan Britz <britzdylan@gmail.com>
2024-01-07 14:01:41 +0100

    feat(appfiles): add shared views, theme files, and Vue files

--- appFiles.ts
+++ appFiles.ts
@@ -25,3 +25,65 @@ export const migrations = [
 export const providers = ['AppProvider', 'SessionDriver/index']
 export const events = ['auth', 'index']
 export const routes = ['api-tokens', 'auth', 'errors', 'profile']
+
+const emails = [
+  'emails/confirmation_template/index',
+  'emails/confirmation_template/plain',
+  'emails/verify_template/index',
+  'emails/verify_template/plain',
+  'emails/welcome/index',
+  'emails/welcome/plain',
+]
+
+const errors = ['errors/not-found', 'errors/server-error', 'errors/unauthorized']
+
+const partials = ['partials/meta']
+
+export const sharedViews = ['app', ...emails, ...errors, ...partials]
+
+export const sharedJs = ['app']
+
+export const sharedCss = ['app']
+
+export const themeFiles = [
+  'theme/avatar/index',
+  'theme/button/index',
+  'theme/card/index',
+  'theme/checkbox/index',
+  'theme/dialog/index',
+  'theme/inputText/index',
+  'theme/menu/index',
+  'theme/menuBar/index',
+  'theme/overlayPanel/index',
+  'theme/toast/index',
+  'theme/global',
+  'theme/index',
+]
+
+export const vueFiles = [
+  'src/Pages/Error',
+  'src/Pages/Login',
+  'src/Pages/Password-reset',
+  'src/Pages/Password-update',
+  'src/Pages/Register',
+  'src/Pages/Verification',
+  'src/Pages/Dashboard/Index',
+  'src/Pages/Dashboard/Profile',
+  'src/Layouts/Auth',
+  'src/Layouts/Default',
+  'src/Components/Composites/EmailVerificationForm',
+  'src/Components/Composites/FlashMessages',
+  'src/Components/Composites/Navbar',
+  'src/Components/Composites/ProfileApiTokens',
+  'src/Components/Composites/ProfileAvatarForm',
+  'src/Components/Composites/ProfileDeleteForm',
+  'src/Components/Composites/ProfileSessionsForm',
+  'src/Components/Composites/ProfileDetailsForm',
+  'src/Components/Composites/ProfilePasswordForm',
+  'src/Components/Overlays/Terms',
+  'src/Components/UI/FormInput',
+  'src/Components/UI/Logo',
+  'src/Components/UI/PageHeader',
+  'src/Components/UI/ProfileMenu',
+  'src/Components/UI/UserAvatar',
+]
